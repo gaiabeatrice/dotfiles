237ab8bf5c2db8d920e1eb34defc4e72a08ec10d
Gaia Beatrice <gaiabeatrice@gmail.com>
2024-06-12 16:43:50 -0600

    change

--- scripts/.local/bin/dotdot/main.go
+++ scripts/.local/bin/dotdot/main.go
@@ -2,25 +2,31 @@ package main
 
 import (
 	"fmt"
-
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type editorFinishedMsg struct{ err error }
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
-func execCommand(name string, args []string) tea.Cmd {
-	c := exec.Command(name, args...)
-	return tea.ExecProcess(c, func(err error) tea.Msg {
-		return editorFinishedMsg{err}
-	})
+type finishedMsg struct{ err error }
+
+type item struct {
+	title, desc string
 }
 
 type model struct {
-	err error
+	list list.Model
+	err  error
 }
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
 
 func (m model) Init() tea.Cmd {
 	return nil
@@ -28,35 +34,57 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		h, _ := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, 20)
+
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "b":
-			return m, execCommand("brew", []string{"bundle", "dump", "-f"})
-		case "i":
-			return m, execCommand("brew", []string{"bundle"})
-		case "c":
-			return m, execCommand("stow", []string{"*/", "--no-folding"})
-		case "ctrl+c", "q":
+		switch keypress := msg.String(); keypress {
+		case "q", "ctrl+c":
 			return m, tea.Quit
-		}
-	case editorFinishedMsg:
-		if msg.err != nil {
-			m.err = msg.err
-			return m, tea.Quit
+
+		case "enter":
+			commandDescription := m.list.SelectedItem().(item).Description()
+			return m, execCommand(commandDescription)
 		}
 	}
-	return m, nil
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+
 }
 
 func (m model) View() string {
-	if m.err != nil {
-		return "Error: " + m.err.Error() + "\n"
-	}
-	return "Press 'b' to dump your brew configuration.\nPress 'i' to install packages with brew.\nPress 'c' to stow your files\nPress 'q' to quit.\n"
+	return docStyle.Render(m.list.View())
+}
+
+func execCommand(command string) tea.Cmd {
+	commandSlice := strings.Split(command, " ")
+	mainCommand := commandSlice[0]
+	sliceLen := len(commandSlice)
+	args := commandSlice[1:sliceLen]
+
+	c := exec.Command(mainCommand, args...)
+	return tea.ExecProcess(c, func(err error) tea.Msg {
+		return finishedMsg{err}
+	})
 }
 
 func main() {
-	m := model{}
+	items := []list.Item{
+		item{title: "Dump brew configuration", desc: "brew bundle dump -f"},
+		item{title: "Install packages with brew", desc: "brew bundle"},
+		item{title: "Stow files", desc: "stow */ --no-folding"},
+	}
+
+	l := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	l.Title = "Choose which command to run"
+	l.SetFilteringEnabled(false)
+	l.SetShowPagination(false)
+
+	m := model{list: l}
+
 	if _, err := tea.NewProgram(m).Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
